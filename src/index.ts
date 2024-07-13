533a82453d13051e5d23a6f4acbd978d9367effe
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-13 13:59:40 -0400

    starting build
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript Server Response");
 });
 
 app.listen(port, () => {
