7fa773a404d69f30b0153d0f1aac19f1a9a54e22
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-16 14:54:45 -0400

    exception test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/handler.ts
+++ src/handler.ts
@@ -2,11 +2,6 @@ import { Request } from 'express';
 
 export default {
     hello: async (req: Request) => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Hello World!'
-            }),
-        };
+        throw new DOMException("Not implemented");
     },
 }
