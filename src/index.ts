f0b6768be56cc3f8322a63f54312f3dbda6514f3
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-16 12:04:25 -0400

    changing to post
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server Response");
+app.post("/", (req: Request, res: Response) => {
+  console.log("POST /");
 });
 
 app.listen(port, () => {
