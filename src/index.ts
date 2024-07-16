f5f7249ca26ddebc62f3cf2e471b2870a54ac6a1
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-07-16 20:12:23 +0000

    Deploying to release from @ dotcomrow/nodejs-gcp-run-tempate@60d6b0c400550f464a7343a5571cb1dd50abc6a6 🚀

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from "express";
+import Handlers from "./handler"
+
+const app: Express = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json()) 
+
+app.post("/", async (req: Request, res: Response) => {
+  const { status, body } = await Handlers.handleRequest(req);
+  res.status(status).setHeader(
+    "Content-Type", "application/json"
+  ).send(body);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
