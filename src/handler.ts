877b51dbcd13af7393b8612c24c37d9a37ee628f
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-16 17:16:55 -0400

    adding security
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/handler.ts
+++ src/handler.ts
@@ -1,18 +1,17 @@
-import { Request } from 'express';
 import { BigQuery } from '@google-cloud/bigquery';
 
 export default {
-    handleRequest: async (req: Request) => {        
+    handleRequest: async (data: any, user: any) => {        
         const bigquery = new BigQuery({
             projectId: process.env.GCP_BIGQUERY_PROJECT_ID,
             keyFilename: '/secrets/google.key'
         });
 
-        
         return {
             status: 200,
             body: {
-                message: "Hello, World!"
+                data,
+                user
             }
         }
     },
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import Handlers from "./handler.js"
+import axios from "axios";
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
@@ -7,7 +8,21 @@ const port = process.env.PORT || 8080;
 app.use(express.json()) 
 
 app.post("/", async (req: Request, res: Response) => {
-  const { status, body } = await Handlers.handleRequest(req);
+  const auth_header = req.header("Authorization");
+  if (!auth_header) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const bearer_token = auth_header.split(" ")[1];
+  var response = await axios.get("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + bearer_token);
+
+  if (response.data.error) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const { status, body } = await Handlers.handleRequest(req.body, response.data);
   res.status(status).setHeader(
     "Content-Type", "application/json"
   ).send(body);
