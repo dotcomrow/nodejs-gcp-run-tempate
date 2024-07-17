681a3be2be2be1e75389bd05f4782b7fdbe21509
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-07-17 01:09:33 +0000

    Deploying to release from @ dotcomrow/nodejs-gcp-run-tempate@8dda277f94983eb2a0c626cf2cc977186942b435 🚀

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import express, { Express, Request, Response } from "express";
+import Handlers from "./handler.js"
+import axios from "axios";
+
+const app: Express = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json()) 
+
+app.options("/", (req: Request, res: Response) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  res.status(200).send();
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  const auth_header = req.header("Authorization");
+  if (!auth_header) {
+    res.status(401).send({
+      message: "Unauthorized"
+    });
+    return;
+  }
+
+  const bearer_token = auth_header.split(" ")[1];
+  var response = await axios.get("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + bearer_token);
+
+  if (response.data.error) {
+    res.status(401).send({
+      message: "Unauthorized"
+    });
+    return;
+  }
+
+  if (response.data.email != process.env.SHARED_SERVICE_ACCOUNT_EMAIL) {
+    res.status(401).send({
+      message: "Unauthorized"
+    });
+    return;
+  }
+
+  const { status, body } = await Handlers.handleRequest(req.body, response.data);
+  res.status(status).setHeader(
+    "Content-Type", "application/json"
+  ).send(body);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
