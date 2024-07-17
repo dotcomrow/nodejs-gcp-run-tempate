8dd8d7c9ae94bf9a522f18a749fd171aed1f8358
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-07-17 11:57:43 +0000

    Deploying to release from @ dotcomrow/nodejs-gcp-run-tempate@d0591d2514618570a3b3931bc51bbbe673563dbf 🚀

---
+++ src/handler.ts
@@ -0,0 +1,26 @@
+import { BigQuery } from '@google-cloud/bigquery';
+import { Parser } from 'aliexpress-parser';;
+
+export default {
+    handleRequest: async (data: any, user: any) => {
+        const bigquery = new BigQuery({
+            projectId: process.env.GCP_BIGQUERY_PROJECT_ID,
+            keyFilename: '/secrets/google.key'
+        });
+
+        
+            var parser = new Parser();
+            parser.search('iphone').then((item) => {
+
+                console.log(item);
+            });
+
+        return {
+            status: 200,
+            body: {
+                data,
+                user
+            }
+        }
+    },
+}
