7d05476c0301e4bc1d6a7311606476fa2923c63d
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-17 14:49:00 -0400

    adding logging
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
 import { BigQuery } from '@google-cloud/bigquery';
-import { Parser } from 'aliexpress-parser';;
+import { Parser } from 'aliexpress-parser';
+import { v4 as uuidv4 } from "uuid";
 
 export default {
     handleRequest: async (data: any, user: any) => {
@@ -8,12 +9,25 @@ export default {
             keyFilename: '/secrets/google.key'
         });
 
-        throw new Error("Not implemented");
-            var parser = new Parser();
-            parser.search('iphone').then((item) => {
+        var search = encodeURIComponent(data.search.replace(/ /g, "-"));
+        var parser = new Parser();
 
-                console.log(item);
+        parser.search(search).then(async (item: any) => {
+            await bigquery.createQueryJob({
+                query: `insert into database_dataset.aliexpress_search_results values (
+                    '` +
+                    data.account_id +
+                    `','` +
+                    data.search_request_id +
+                    `','` +
+                    uuidv4() +
+                    `',
+                    PARSE_JSON('` +
+                    JSON.stringify(item) +
+                    `'),
+                    CURRENT_TIMESTAMP())`
             });
+        });
 
         return {
             status: 200,
