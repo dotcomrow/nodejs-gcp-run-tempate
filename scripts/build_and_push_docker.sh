a4c7a2d1c8960e703c66920f0afe8106c60fc576
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-21 18:45:13 -0400

    adding pod
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- scripts/build_and_push_docker.sh
+++ scripts/build_and_push_docker.sh
@@ -28,6 +28,7 @@ chmod -R go-w "$(brew --prefix)/share/zsh"
 
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install svn
 brew install podman
 
 
