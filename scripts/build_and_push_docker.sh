ebb3fa78d453bc61a41301843b7f99f6df226bb1
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-21 15:22:13 -0400

    hmm
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- scripts/build_and_push_docker.sh
+++ scripts/build_and_push_docker.sh
@@ -1,5 +1,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+apt-get install -y uidmap
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
